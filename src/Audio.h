8b089c2899c0675837390c8eed36b3237a6ae814
Bernifoxjedi <145194881+Bernifoxjedi@users.noreply.github.com>
2024-12-06 09:34:07 +0100

    audio.h , .cpp trys

--- src/Audio.h
+++ src/Audio.h
@@ -1,40 +1,39 @@
 #pragma once
 
+#include "Module.h"
 #include "SDL2/SDL_mixer.h"
-#include "pugixml.hpp"
-#include <string>
-#include <unordered_map>
+#include <list>
 
-class Audio {
+#define DEFAULT_MUSIC_FADE_TIME 2.0f
+
+struct _Mix_Music;
+
+class Audio : public Module
+{
 public:
-    enum class SoundType {
-        FX,     
-        MUSIC   
-    };
-    std::string name;        
-    std::string path;         
-    SoundType type;           
-    Mix_Chunk* chunk = nullptr;  
-    Mix_Music* music = nullptr; 
+
+	Audio();
+
+	// Destructor
+	virtual ~Audio();
+
+	// Called before render is available
+	bool Awake();
+
+	// Called before quitting
+	bool CleanUp();
+
+	// Play a music file
+	bool PlayMusic(const char* path, float fadeTime = DEFAULT_MUSIC_FADE_TIME);
+
+	// Load a WAV in memory
+	int LoadFx(const char* path);
+
+	// Play a previously loaded WAV
+	bool PlayFx(int fx, int repeat = 0);
+
 private:
-    
 
-public:
-    Audio() = default;
-    ~Audio();
-
-    
-    void LoadFromXML(pugi::xml_node soundNode);
-
-    
-    bool Play(int loops = 0) const;
-
-    
-    static void Stop();
-
-    
-    static void Pause();
-
-    
-    static void Resume();
+	_Mix_Music* music;
+	std::list<Mix_Chunk*> fx;
 };
